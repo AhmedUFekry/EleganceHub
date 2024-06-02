34f2893fa82dcb4addf7cf986715bcf445b2e104
Shimaasami400 <114086723+Shimaasami400@users.noreply.github.com>
2024-06-02 09:32:14 +0300

    fixing bug

--- EleganceHub/AppDelegate.swift
+++ EleganceHub/AppDelegate.swift
@@ -8,49 +8,29 @@
 import UIKit
 import FirebaseCore
 
-//@main
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-//
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//            // Override point for customization after application launch.
-//        FirebaseApp.configure()
-//            return true
-//        }
-//
-//        // MARK: UISceneSession Lifecycle
-//
-//        func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-//            // Called when a new scene session is being created.
-//            // Use this method to select a configuration to create the new scene with.
-//            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-//        }
-//
-//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set) {
-//        // Called when the user discards a scene session.
-//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-//    }
-//
-//
-//
-//}
-
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            // Override point for customization after application launch.
+        FirebaseApp.configure()
+            return true
+        }
 
-        // Create an instance of SearchViewController
-        let searchViewController = SearchViewController()
+        // MARK: UISceneSession Lifecycle
 
-        // Create a window and set the root view controller
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = searchViewController
-        window?.makeKeyAndVisible()
+        func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        }
 
-        return true
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-}
 
+
+
+}
