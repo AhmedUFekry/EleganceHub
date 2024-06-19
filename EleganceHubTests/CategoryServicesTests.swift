14b77421b5d44b2c870c2a36ddba2961103cb59d
Raneem Ashraf <56923695+raneemashraf@users.noreply.github.com>
2024-06-19 21:44:13 +0300

    test Expectation 🫡

---
+++ EleganceHubTests/CategoryServicesTests.swift
@@ -0,0 +1,78 @@
+//
+//  CategoryServicesTests.swift
+//  EleganceHubTests
+//
+//  Created by raneem on 19/06/2024.
+//
+
+import XCTest
+@testable import EleganceHub
+
+final class CategoryServicesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGetWomenCategory(){
+        let brandsExpectation = expectation(description:"waiting for api")
+        RemoteDataSource.shared.getCategoryProducts(collectionId: "484444307731", completionHandler: { products,error in
+            if let error = error {
+                XCTFail("API call failed with error: \(error.localizedDescription)")
+            }else {
+                XCTAssertNotNil(products?.products)
+                XCTAssertEqual(products?.products?.count,4)
+                brandsExpectation.fulfill()
+            }
+            
+        })
+        waitForExpectations(timeout: 10)
+    }
+
+    func testGetMenCategory(){
+        let brandsExpectation = expectation(description:"waiting for api")
+        RemoteDataSource.shared.getCategoryProducts(collectionId: "484444274963", completionHandler: { products,error in
+            if let error = error {
+                XCTFail("API call failed with error: \(error.localizedDescription)")
+            }else {
+                XCTAssertNotNil(products?.products)
+                XCTAssertEqual(products?.products?.count,36)
+                brandsExpectation.fulfill()
+            }
+            
+        })
+        waitForExpectations(timeout: 10)
+    }
+    func testGetKidsCategory(){
+        let brandsExpectation = expectation(description:"waiting for api")
+        RemoteDataSource.shared.getCategoryProducts(collectionId: "484444340499", completionHandler: { products,error in
+            if let error = error {
+                XCTFail("API call failed with error: \(error.localizedDescription)")
+            }else {
+                XCTAssertNotNil(products?.products)
+                XCTAssertEqual(products?.products?.count,6)
+                brandsExpectation.fulfill()
+            }
+            
+        })
+        waitForExpectations(timeout: 10)
+    }
+    func testGetSaleCategory(){
+        let brandsExpectation = expectation(description:"waiting for api")
+        RemoteDataSource.shared.getCategoryProducts(collectionId: "484444406035", completionHandler: { products,error in
+            if let error = error {
+                XCTFail("API call failed with error: \(error.localizedDescription)")
+            }else {
+                XCTAssertNotNil(products?.products)
+                XCTAssertEqual(products?.products?.count,3)
+                brandsExpectation.fulfill()
+            }
+            
+        })
+        waitForExpectations(timeout: 10)
+    }
+}
--- EleganceHubTests/EleganceHubTests.swift
+++ EleganceHubTests/EleganceHubTests.swift
@@ -36,92 +36,12 @@ final class EleganceHubTests: XCTestCase {
         }
     }
     
-    func testGetBrands(){
-        let brandsExpectation = expectation(description:"waiting for api")
-        RemoteDataSource.shared.getBrands(complationhandler:{result,error in
-            if let error = error {
-                XCTFail("API call failed with error: \(error.localizedDescription)")
-            }else {
-                XCTAssertNotNil(result?.smartCollections)
-                brandsExpectation.fulfill() // done
-            }
-            
-        })
-        waitForExpectations(timeout: 25)
-    }
+   
     
-    func testGetProducts(){
-        let brandsExpectation = expectation(description:"waiting for api")
-        RemoteDataSource.shared.getProducts(collectionId: 484442276115, completionHandler: { products,error in
-            if let error = error {
-                XCTFail("API call failed with error: \(error.localizedDescription)")
-            }else {
-                XCTAssertNotNil(products?.products)
-                XCTAssertEqual(products?.products?.count,3)
-                brandsExpectation.fulfill()
-            }
-            
-        })
-        waitForExpectations(timeout: 30)
-    }
     
-    func testGetCategories(){
-        let brandsExpectation = expectation(description:"waiting for api")
-        RemoteDataSource.shared.getCategoryProducts(collectionId: "484444307731", completionHandler: { products,error in
-            if let error = error {
-                XCTFail("API call failed with error: \(error.localizedDescription)")
-            }else {
-                XCTAssertNotNil(products?.products)
-                XCTAssertEqual(products?.products?.count,4)
-                brandsExpectation.fulfill()
-            }
-            
-        })
-        waitForExpectations(timeout: 10)
-    }
     
-    func testGetOrders(){
-        let brandsExpectation = expectation(description:"waiting for api")
-        ordersService.getOrders(customerId: "8268495585555", completionHandler:{ orders,error in
-            if let error = error {
-                XCTFail("API call failed with error: \(error.localizedDescription)")
-            }else {
-                XCTAssertNotNil(orders?.orders)
-               // XCTAssertEqual(orders?.orders?.count,1)
-                brandsExpectation.fulfill()
-            }
-            
-        })
-        waitForExpectations(timeout: 10)
-    }
-    func testGetDraftOrders(){
-        let brandsExpectation = expectation(description:"waiting for api")
-        ordersService.getDraftOrderForUser(orderID: 1157792760083, completionHandler:{ draftOrders,error in
-            if let error = error {
-                XCTFail("API call failed with error: \(error.localizedDescription)")
-            }else {
-                XCTAssertNotNil(draftOrders?.draftOrders)
-               // XCTAssertEqual(draftOrders?.draftOrders.count,1)
-                brandsExpectation.fulfill()
-            }
-        })
-        waitForExpectations(timeout: 10)
-    }
+   
     
-//  func testDeleteOrder() {
-//            let expectation = self.expectation(description: "waiting for api")
-//
-//            ordersService.deleteOrder(orderID: 5844130070803) { statusCode in
-//                XCTAssertEqual(statusCode, 200)
-//                expectation.fulfill()
-//            }
-//
-//            waitForExpectations(timeout: 10) { error in
-//                if let error = error {
-//                    XCTFail("Test timed out with error: \(error.localizedDescription)")
-//                }
-//            }
-//        }
     
     
 }
